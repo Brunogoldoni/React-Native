734bfdd8a48e38e4f8e63528bfd189ce30c27f4b
bruno <bgoldoni@blupay.com.br>
2021-06-23 12:23:08 -0300

    Folder Structure

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+import { colors } from "../../styles/colors";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.black};
+`;
+
+export const Title = styled.Text`
+  color: ${colors.white};
+`;
